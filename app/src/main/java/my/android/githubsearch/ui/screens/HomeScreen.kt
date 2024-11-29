9073eeb33a2cf40966f880e53bf6e3485cd5675c
ayushsinha-1 <143383811+ayushsinha-1@users.noreply.github.com>
2024-11-29 19:53:57 +0530

    Modified Home Screen UI for view model

--- app/src/main/java/my/android/githubsearch/ui/screens/HomeScreen.kt
+++ app/src/main/java/my/android/githubsearch/ui/screens/HomeScreen.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,35 +24,60 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import my.android.githubsearch.data.model.Repository
-import my.android.githubsearch.data.model.RepositoryOwner
 import my.android.githubsearch.ui.screens.components.RepositoryCard
+import my.android.githubsearch.ui.viewmodel.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-   onRepositoryClick: (Repository) -> Unit
+    homeViewModel: HomeViewModel,
+    onRepositoryClick: (Repository) -> Unit
 ) {
-    var searchQuery by remember { mutableStateOf("") }
-    val isLoading by remember { mutableStateOf(false) }
+    val repositories by homeViewModel.repositories.collectAsState()
+    val isLoading by homeViewModel.isLoading.collectAsState()
+    val error by homeViewModel.error.collectAsState()
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    var searchQuery by rememberSaveable { mutableStateOf("") }
+    val listState = rememberLazyListState()
+    var hasSearched by remember { mutableStateOf(false) }
+
+    LaunchedEffect(error) {
+        error?.let { errorMessage ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(errorMessage)
+            }
+        }
+    }
 
     Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             TopAppBar(
                 title = { Text("GitHub Repository Search") },
@@ -68,23 +96,32 @@ fun HomeScreen(
             SearchBar(
                 searchQuery = searchQuery,
                 onSearchQueryChange = { searchQuery = it },
-                onSearch = { },
+                onSearch = {
+                    hasSearched = true
+                    homeViewModel.searchRepositories(searchQuery) },
+                onCrossClick = {
+                    searchQuery = ""
+                    hasSearched = false
+                    homeViewModel.resetSearch()
+                },
                 isLoading = isLoading
             )
 
-            if (isLoading) {
+            if (isLoading && repositories.isEmpty()) {
                 Box(
                     modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
                 ) {
                     CircularProgressIndicator()
                 }
-            } else if (repositories.isEmpty()) {
-                EmptyResult()
             } else {
                 RepositoryList(
                     repositories = repositories,
-                    onRepositoryClick = onRepositoryClick
+                    listState = listState,
+                    isLoading = isLoading,
+                    hasSearched = hasSearched,
+                    onRepositoryClick = onRepositoryClick,
+                    onLoadMore = { homeViewModel.loadMoreRepositories() }
                 )
             }
         }
@@ -96,6 +133,7 @@ private fun SearchBar(
     searchQuery: String,
     onSearchQueryChange: (String) -> Unit,
     onSearch: () -> Unit,
+    onCrossClick:() -> Unit,
     isLoading: Boolean
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
@@ -121,13 +159,17 @@ private fun SearchBar(
             .padding(16.dp),
         trailingIcon = {
             IconButton(
-                onClick = {
-                    onSearch()
-                    keyboardController?.hide()
-                },
-                enabled = !isLoading
+                onClick =  {
+                    if (searchQuery.isNotEmpty()) {
+                        onCrossClick()
+                        keyboardController?.hide()
+                    }
+                }
             ) {
-                Icon(Icons.Default.Search, contentDescription = "Search")
+                Icon(
+                    imageVector = if (searchQuery.isEmpty()) Icons.Default.Search else Icons.Default.Close,
+                    contentDescription = if (searchQuery.isEmpty()) "Search" else "Clear Search"
+                )
             }
         },
         colors = OutlinedTextFieldDefaults.colors(
@@ -140,20 +182,74 @@ private fun SearchBar(
 @Composable
 private fun RepositoryList(
     repositories: List<Repository>,
-    onRepositoryClick: (Repository) -> Unit
+    listState: LazyListState,
+    isLoading: Boolean,
+    hasSearched: Boolean,
+    onRepositoryClick: (Repository) -> Unit,
+    onLoadMore: () -> Unit
 ) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(repositories) { repo ->
-            RepositoryCard(
-                repository = repo,
-                onClick = { onRepositoryClick(repo) },
-                modifier = Modifier.fillMaxWidth()
-            )
+    when {
+        repositories.isEmpty() && !isLoading && !hasSearched -> {
+            InitialMessage()
         }
+        repositories.isEmpty() && !isLoading -> {
+            EmptyResult()
+        }
+        else -> {
+            LazyColumn(
+                state = listState,
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                verticalArrangement = Arrangement.spacedBy(14.dp)
+            ) {
+                items(repositories) { repo ->
+                    RepositoryCard(
+                        repository = repo,
+                        onClick = { onRepositoryClick(repo) },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+                if (isLoading) {
+                    item {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator()
+                        }
+                    }
+                }
+            }
+
+            LaunchedEffect(listState) {
+                snapshotFlow { listState.layoutInfo }
+                    .collect { layoutInfo ->
+                        val totalItems = layoutInfo.totalItemsCount
+                        val lastVisibleItemIndex = layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: 0
+
+                        if (lastVisibleItemIndex >= totalItems - 5 && !isLoading) {
+                            onLoadMore()
+                        }
+                    }
+            }
+        }
+    }
+}
+
+@Composable
+private fun InitialMessage() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Welcome!\nSearch for repositories to get started.",
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
@@ -169,54 +265,5 @@ private fun EmptyResult() {
             fontWeight = FontWeight.Medium,
             textAlign = TextAlign.Center
         )
-    }
-}
-
-val repositories = listOf (
-    Repository(
-        id = 123456,
-        name = "LearnKotlin",
-        full_name = "ayush/learn-kotlin",
-        description = "This is the best repository to learn Kotlin.",
-        html_url = "https://github.com/ayush/learn-kotlin",
-        stargazers_count = 1500,
-        language = "Kotlin",
-        forks_count = 250,
-        updated_at = "24/11/24",
-        watchers_count = 2423,
-        open_issues_count = 45,
-        isOfflineCache = false,
-        owner = RepositoryOwner(
-            avatar_url = "https://avatars.githubusercontent.com/u/143383811?v=4",
-            html_url = "https://github.com/ayush19sinha",
-            login = "Ayush"
-        )
-    ),
-    Repository(
-        id = 321321,
-        name = "LearnJava",
-        full_name = "ayush/learn-Java",
-        description = "This is the best repository to learn Java.",
-        html_url = "https://github.com/ayush/learn-Java",
-        stargazers_count = 1837,
-        language = "Java",
-        forks_count = 290,
-        updated_at = "24/11/24",
-        watchers_count = 141,
-        open_issues_count = 123,
-        isOfflineCache = false,
-        owner = RepositoryOwner(
-            avatar_url = "",
-            html_url = "https://github.com/ayush19sinha",
-            login = "Ayush"
-        )
-    ),
-)
-
-@Preview(showBackground = true)
-@Composable
-private fun HomeScreenPreview() {
-    MaterialTheme {
-        HomeScreen(onRepositoryClick = {})
     }
 }
